c885930e959d298ac9268efc00f59155ccb47745
Hassan Mehmood <hassanmehmood.psr@gmail.com>
2022-09-29 14:07:55 +0500

    Signup has been completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = 'Account has been created successfully'
-      redirect_to @article
+      flash[:notice] = "Welcome #{@user.username}. Your account has been created successfully"
+      redirect_to articles_path
 
     else
       render :new, status: :unprocessable_entity
 
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
